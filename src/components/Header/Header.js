0b2560fc054b4f54ea0f81bff4a8e683a1c10c20
Your Name <you@example.com>
2019-09-14 23:38:44 +0200

    feat: basic structure

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import "./styles.scss"
+
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                my header
+            </header>
+        )
+    }
+}
