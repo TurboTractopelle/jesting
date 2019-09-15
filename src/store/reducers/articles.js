4488e2b98f3fc1ae763e1630911b77dc7ebae43e
neroark <ns_ark@hotmail.com>
2019-09-15 16:24:10 +0200

    feat: set actionTypes and reducer

--- src/store/reducers/articles.js
+++ src/store/reducers/articles.js
@@ -1,14 +1,21 @@
+import * as actionTypes from "../actions/actionTypes";
 
-const initialState = []
-const articles = (state = initialState, action)=>{
-    switch (action.type) {
-        case "a":
-            return state
-    
-        default:
-            return state
-    }
+const initialState = {
+  data: [],
+  loading: false,
+  error: false
+};
+const articles = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.FETCH_SUCCESS:
+      return { ...state, data: action.data, loading: false };
+    case actionTypes.FETCH_START:
+      return { ...state, loading: true };
+    case actionTypes.FETCH_FAILURE:
+      return { ...state, loading: false, error: true };
+    default:
+      return state;
+  }
+};
 
-}
-
-export default articles
+export default articles;
--- src/store/reducers/articles.test.js
+++ src/store/reducers/articles.test.js
@@ -1,8 +1,8 @@
-import articles from "./articles"
+import articles from "./articles";
 
-describe('articles reducer', () => {
-    it("returns the state with default action", ()=>{
-        const newState = articles(undefined, {type:"stuff"})
-        expect(newState).toEqual([])
-    })
-})
+describe("articles reducer", () => {
+  it("returns the state with default action", () => {
+    const newState = articles(undefined, { type: "stuff" });
+    expect(newState).toEqual({ data: [], loading: false, error: false });
+  });
+});
