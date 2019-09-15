f04597c954165a75f5373464f5712bbcb56bfa65
Your Name <you@example.com>
2019-09-15 16:13:56 +0200

    feat: testing reducer

--- src/store/reducers/articles.js
+++ src/store/reducers/articles.js
@@ -4,13 +4,10 @@ const articles = (state = initialState, action)=>{
     switch (action.type) {
         case "a":
             return state
-
     
         default:
             return state
     }
-
-
 
 }
 
---
+++ src/store/reducers/articles.test.js
@@ -0,0 +1,8 @@
+import articles from "./articles"
+
+describe('articles reducer', () => {
+    it("returns the state with default action", ()=>{
+        const newState = articles(undefined, {type:"stuff"})
+        expect(newState).toEqual([])
+    })
+})
